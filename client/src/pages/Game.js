30d1eb033eee0370b8adbaa47f31748db21dd9ab
Pihonak18 <64816908+Pihonak18@users.noreply.github.com>
2020-10-21 19:15:38 -0400

    Update Game.js

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,38 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import questions from "./questions.json";
 
+
 function Game() {
-  const [showModal, setShowModal] = useState(true);
-  const [questiondata, setQuestion] = useState("");
-  const [questiontitle, setTitle] = useState("");
-  const [timeLeft, setTimeLeft] = useState(null);
-  useEffect(() => {
-    if (timeLeft === 0) {
-      console.log("TIME LEFT IS 0");
-      setTimeLeft(null);
-      setShowModal(false);
-    }
-
-    // exit early when we reach 0
-    if (!timeLeft) return;
-
-    // save intervalId to clear the interval when the
-    // component re-renders
-    const intervalId = setInterval(() => {
-      setTimeLeft(timeLeft - 1);
+  const [showModal, setShowModal] = React.useState(true);
+  const [questiondata, setQuestion] = React.useState("");
+  const [timer, setTimer] = React.useState(15);
+  const [answers, setAnswers] = React.useState("");
+  const [score, setScore] =React.useState("");
+  
+  //a hook to show or hide the modal when it has been clicked by a player or is still available
+  let time = 20;
+  const Timer = () => {
+    setInterval(function countdown() {
+      time -= 1;
+      setTimer(time);
+      if (time === 0) {
+        
+        setShowModal(false);
+      }
     }, 1000);
+  };
 
-    // clear interval on re-render to avoid memory leaks
-    return () => clearInterval(intervalId);
-    // add timeLeft as a dependency to re-rerun the effect
-    // when we update it
-  }, [timeLeft]);
+  
 
+  //questions array of objs...attributes wasclicked boolean,false by default, if true disable
+  //set diasble t
   return (
     <div className="wrapper">
       <div className="game-wrapper">
         <button className="button">Category</button>
-        <button className="button" data-toggle="modal">
+        <button onclick="setTimeout(myFunction, 3000)" className="button" data-toggle="modal">
           Category
         </button>
         <button className="button" data-toggle="modal">
@@ -41,18 +39,23 @@ function Game() {
         <button className="button" data-toggle="modal">
           Category
         </button>
+          
+        
 
-        {questions.map(({ id, question, score, title }, index) => (
+        {questions.map(({ id, question, score, wasClicked, answers }, index) => (
           <button
             key={id}
             className="game-button"
             data-toggle="modal"
             data-target="#exampleModal"
-            // disabled={wasClicked}
+            disabled={wasClicked}
             onClick={() => {
-              setTimeLeft(15);
-              setTitle(title);
+              Timer();
               setQuestion(question);
+              setAnswers(answers);
+              
+
+
             }}
           >
             {score}
@@ -60,29 +63,11 @@ function Game() {
         ))}
       </div>
 
-      <div className="card-group">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Player</h5>
-            <p className="card-text">score</p>
-            <button type="button" className="btn btn-danger">
-              Give Up
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Player</h5>
-            <p className="card-text">score</p>
-            <button type="button" className="btn btn-danger">
-              Give Up
-            </button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Player</h5>
-            <p className="card-text">score</p>
+      <div class="card-group">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Player</h5>
+          <p class="card-text">Score:{score}</p>
             <button type="button" className="btn btn-danger">
               Give Up
             </button>
@@ -98,37 +83,40 @@ function Game() {
           role="dialog"
           aria-labelledby="exampleModalLabel"
         >
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">
-                  {timeLeft} {questiontitle}
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  {timer} Question Title
                 </h5>
                 <button
                   type="button"
-                  className="close"
+                  class="close"
                   data-dismiss="modal"
                   aria-label="Close"
                 >
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div className="modal-body">{questiondata}</div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-dismiss="modal"
-                >
-                  Close
-                </button>
+      <div class="modal-body">{questiondata}</div>
+      <button id="bttn" class="button">{answers[0]}</button>
+      <button id="bttn" class="button">{answers[1]}</button>
+      <button id="bttn" class="button">{answers[2]}</button>
+      <button id="bttn" class="button">{answers[3]}</button>
+      
+              <div class="modal-footer">
+                
               </div>
+              
             </div>
           </div>
         </div>
       ) : null}
     </div>
+    
   );
+          
 }
+
 
 export default Game;
