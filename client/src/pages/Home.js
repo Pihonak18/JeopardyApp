b8afa56f07128d0cb33f6cad46c1542006c23bb5
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-26 22:14:41 -0400

    CSS

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
-
+import "./style.css"
 class Home extends Component {
   render() {
+    const readybanner = {
+      color: "white",
+      background: "DodgerBlue",
+      width: "200px",
+      padding: "25px",
+      padding: "10px",
+      border: "25px solid navy"
+    };
+    const readybannerh2 = {
+      color: "white",
+      background: "DodgerBlue",
+      width: "200px",
+      padding: "25px",
+      padding: "10px",
+      border: "25px solid navy"
+    };
     return (
-      <div>
-        <h2 id="ready-banner" className="p-3 mb-2 bg-primary text-white ready-banner">
+      <div style={{readybanner}}>
+        {/*<h2 style={{color: "red", background: "green"}}>*/}
+        <h2 style={{color: "white", background:	"rgba(2, 117, 216, 0.75", padding: "10px",}}>
         {/* <h2 id="ready-banner" className="p-3 mb-2 bg-primary text-white ready-banner"> */}
         {/* <h2 id="ready-banner" className="text-white ready-banner"> */}
           Are you Ready to Play Jeopardy?
@@ -12,7 +29,7 @@ class Home extends Component {
         <img
           src="https://i.ibb.co/mvvMCpD/America-jeopardy-2019-web-app.png"
           height="370"
-          alt="jeopardy tag"
+          alt="jeopardy tag"          
         />
       </div>
     );
