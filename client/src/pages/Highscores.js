14977b5fa00c8db3564322104930c2207ba25591
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-28 18:13:52 -0400

    Fixed CSS on the Scores Page

--- client/src/pages/Highscores.js
+++ client/src/pages/Highscores.js
@@ -17,7 +17,7 @@ function Highscores() {
   };
   return (
     <div>
-      <h1 className="highscoresh1">
+      <h1 className="scores-list">
         {" "}
         Below is a list of all the highscores and the players who earned them!{" "}
         {highscores.map((scores) => (
